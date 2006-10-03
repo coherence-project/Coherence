e134fde9185b7710f90e67f2e8371e597f210d40
Frank Scholz <dev@coherence-project.org>
2006-10-03 21:40:32 +0000

    added StateVariable (auto-)update on return of an action call, refs #6

--- python-upnp/action.py
+++ python-upnp/action.py
@@ -61,17 +61,33 @@ class Action:
 
     def get_arguments_list(self):
         return self.arguments_list
-
+        
+    def get_in_arguments(self):
+        return [arg for arg in self.arguments_list if arg.get_direction() == 'in']
+        
+    def get_out_arguments(self):
+        return [arg for arg in self.arguments_list if arg.get_direction() == 'out']
+            
     def get_service(self):
         return self.service
 
     def call(self, *args, **kwargs):
-        print "calling", self.name
+        #print "calling", self.name
+        in_arguments = self.get_in_arguments()
+        #print "in arguments", [a.get_name() for a in in_arguments]
         instance_id = 0
         for arg_name, arg in kwargs.iteritems():
-            print arg_name, arg
+            l = [ a for a in in_arguments if arg_name == a.get_name()] 
+            if len(l) > 0:
+                in_arguments.remove(l[0])
+            else:
+                print "argument %s not valid for action %s" % (arg_name,self.name)
+                return
             if arg_name == 'InstanceID':
                 instance_id = arg
+        if len(in_arguments) > 0:
+            print "argument %s missing for action %s" % ([ a.get_name() for a in in_arguments],self.name)
+            return
         client = self._get_client()
         d = client.callRemote( self.name,
                                     **kwargs)
@@ -79,7 +95,16 @@ class Action:
         return d
 
     def got_results( self, results, instance_id):
-        print "call %s (instance %d) returns: %r" % (self.name, instance_id, results)
+        #print "call %s (instance %d) returns: %r" % (self.name, instance_id, results)
+        out_arguments = self.get_out_arguments()
+        if len(out_arguments) == 0:
+            pass
+        elif len(out_arguments) == 1:
+            self.service.get_state_variable(out_arguments[0].get_state_variable(), instance_id).update(results)
+        else:
+            for arg_name, value in results._asdict().items():
+                state_variable_name = [a.get_state_variable() for a in out_arguments if a.get_name() == arg_name]
+                self.service.get_state_variable(state_variable_name[0], instance_id).update(value)
         
     def __repr__(self):
         return "Action: %s (%s args)" % (self.get_name(),
