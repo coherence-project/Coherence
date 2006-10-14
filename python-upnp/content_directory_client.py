b250713a46f9d7f5d9943c5c2c36156b039ba298
Frank Scholz <dev@coherence-project.org>
2006-10-14 14:46:45 +0000

    Adjust licence, address #7

--- python-upnp/content_directory_client.py
+++ python-upnp/content_directory_client.py
@@ -1,23 +1,8 @@
-# Elisa - Home multimedia server
+# Licensed under the MIT license
+# http://opensource.org/licenses/mit-license.php
+ 	
 # Copyright (C) 2006 Fluendo, S.A. (www.fluendo.com).
-# All rights reserved.
-# 
-# This software is available under three license agreements.
-# 
-# There are various plugins and extra modules for Elisa licensed
-# under the MIT license. For instance our upnp module uses this license.
-# 
-# The core of Elisa is licensed under GPL version 2.
-# See "LICENSE.GPL" in the root of this distribution including a special 
-# exception to use Elisa with Fluendo's plugins.
-# 
-# The GPL part is also available under a commerical licensing
-# agreement.
-# 
-# The second license is the Elisa Commercial License Agreement.
-# This license agreement is available to licensees holding valid
-# Elisa Commercial Agreement licenses.
-# See "LICENSE.Elisa" in the root of this distribution.
+# Copyright 2006, Frank Scholz <coherence@beebits.net>
 
 from twisted.internet import reactor, defer
 from twisted.python import log
