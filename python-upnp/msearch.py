7991fc433c5915e48b2553cfcd66fe2df18e3935
Frank Scholz <dev@coherence-project.org>
2006-10-14 14:24:04 +0000

    Adjust loop time for msearch, now do searches every 120 seconds. Maybe that's still too often.
    Add the manifestation argument to the SSDP.register call and modified the licence.

--- python-upnp/msearch.py
+++ python-upnp/msearch.py
@@ -1,24 +1,8 @@
-# Elisa - Home multimedia server
+# Licensed under the MIT license
+# http://opensource.org/licenses/mit-license.php
+#
 # Copyright (C) 2006 Fluendo, S.A. (www.fluendo.com).
-# All rights reserved.
-# 
-# This software is available under three license agreements.
-# 
-# There are various plugins and extra modules for Elisa licensed
-# under the MIT license. For instance our upnp module uses this license.
-# 
-# The core of Elisa is licensed under GPL version 2.
-# See "LICENSE.GPL" in the root of this distribution including a special 
-# exception to use Elisa with Fluendo's plugins.
-# 
-# The GPL part is also available under a commerical licensing
-# agreement.
-# 
-# The second license is the Elisa Commercial License Agreement.
-# This license agreement is available to licensees holding valid
-# Elisa Commercial Agreement licenses.
-# See "LICENSE.Elisa" in the root of this distribution.
-
+# Copyright 2006, Frank Scholz <coherence@beebits.net>
         
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
@@ -36,17 +20,18 @@ class MSearch(DatagramProtocol):
         port = reactor.listenUDP(5654, self)
 
         l = task.LoopingCall(self.double_discover)
-        l.start(50.0)
+        l.start(120.0)
 
     def datagramReceived(self, data, (host, port)):
         cmd, headers = utils.parse_http_response(data)
 
         if cmd[0] == 'HTTP/1.1' and cmd[1] == '200':
             if not self.ssdp_server.isKnown(headers['usn']):
-                self.ssdp_server.register(headers['usn'], headers['st'],
-                                          headers['location'],
-                                          headers['server'],
-                                          headers['cache-control'])
+                self.ssdp_server.register('remote',
+                                            headers['usn'], headers['st'],
+                                            headers['location'],
+                                            headers['server'],
+                                            headers['cache-control'])
 
     def double_discover(self):
         " Because it's worth it (with UDP's reliability) "
