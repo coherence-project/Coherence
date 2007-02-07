e6d22fde950894314f2904d1022e7fdf0f752938
Frank Scholz <dev@coherence-project.org>
2007-02-07 15:41:14 +0000

    correct version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name="Coherence",
-      version="0.2",
+      version="0.1",
       description="""Coherence - Python framework for the digital living""",
       author="Frank Scholz",
       author_email='coherence@beebits.net',
