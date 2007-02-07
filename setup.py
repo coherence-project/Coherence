f67c207ba26613108d5da02a0d72b5cfd3e0cbc8
Philippe Normand <phil@base-art.net>
2007-02-07 14:37:02 +0000

    initial setup.py checkin

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import sys
+import os
+
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+      name="coherence",
+      version="0.1",
+      description="""Coherence quoi""",
+      author="not me",
+      packages=find_packages()
+      )
