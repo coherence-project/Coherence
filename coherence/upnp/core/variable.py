a7b34a073c8ef2b849cb675395f67cf1435e0e56
Frank Scholz <dev@coherence-project.org>
2007-02-09 13:09:57 +0000

    follow up to [292]

--- coherence/upnp/core/variable.py
+++ coherence/upnp/core/variable.py
@@ -94,7 +94,10 @@ class StateVariable:
                         self.value = value
             else:
                 if self.data_type == 'string':
-                    value = str(value)
+                    if type(value) == unicode:
+                        value = value.encode('utf-8')
+                    else:
+                        value = str(value)
                     if len(self.allowed_values):
                         if self.has_vendor_values == True:
                             self.value = value
