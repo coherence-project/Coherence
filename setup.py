de370b990a68e84fd58a72f5ac2645ae77909deb
Frank Scholz <dev@coherence-project.org>
2007-02-16 09:45:25 +0000

    version change in setup.py from 0.1 to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name="Coherence",
-      version="0.1",
+      version="0.1.0",
       description="""Coherence - Python framework for the digital living""",
       author="Frank Scholz",
       author_email='coherence@beebits.net',
