af72d3d18d04b083d5fd93213da8a654d69700db
Frank Scholz <dev@coherence-project.org>
2007-02-19 20:48:38 +0000

    cleanups and a simple test to retrieve the state info from the Elisa player

--- coherence/backends/elisa_renderer.py
+++ coherence/backends/elisa_renderer.py
@@ -38,6 +38,7 @@ class ElisaPlayer:
         self.name = kwargs.get('name','Elisa MediaRenderer')
         self.host = kwargs.get('host','localhost')
         self.player = None
+        
         if self.host == 'internal':
             try:
                 from elisa.core import common
@@ -64,61 +65,39 @@ class ElisaPlayer:
 
     
         self.playing = False
+        self.state = None
         self.duration = None
         self.view = []
         self.tags = {}
         self.server = device
+        self.poll_LC = LoopingCall( self.poll_player)
 
+        
     def __repr__(self):
         return str(self.__class__).split('.')[-1]
 
-    def poll_gst_bus( self):
-        # FIXME: isn't there any better way to do this?
-        #print 'poll_gst_bus'
-        while True:
-            # FIXME: maybe a counter, so we don't stay to long in here?
-            message = self.bus.poll(gst.MESSAGE_ERROR|gst.MESSAGE_EOS| \
-                                        gst.MESSAGE_TAG|gst.MESSAGE_STATE_CHANGED,
-                                    timeout=1)
-            if message == None:
-                return
-            self.on_message(self.bus, message)
-            
-    def on_message(self, bus, message):
-        #print "on_message", message
-        #print "from", message.src.get_name()
-        t = message.type
-        #print t
-        if t == gst.MESSAGE_ERROR:
-            err, debug = message.parse_error()
-            print "Gstreamer error: %s" % err, debug
-            if self.playing == True:
-                self.seek('-0')
-            #self.player.set_state(gst.STATE_READY)
-        elif t == gst.MESSAGE_TAG:
-            for key in message.parse_tag().keys():
-                self.tags[key] = message.structure[key]
-            #print self.tags
-        elif t == gst.MESSAGE_STATE_CHANGED:
-            if message.src == self.player:
-                old, new, pending = message.parse_state_changed()
-                #print "player (%s) state_change:" %(message.src.get_path_string()), old, new, pending
-                if new == gst.STATE_PLAYING:
-                    self.playing = True
-                    self.update_LC.start( 1, False)
-                    self.update()
-                elif old == gst.STATE_PLAYING:
-                    self.playing = False
-                    self.update_LC.stop()
-                    self.update()
-                #elif new == gst.STATE_READY:
-                #    self.update()
+    def poll_player( self):
+        def got_result(result):
+            print result
+            if self.server != None:
+                connection_id = self.server.connection_manager_server.lookup_avt_id(self.current_connection_id)
+            if result == 'STOPPED':
+                transport_state = 'STOPPED'
+            if result == 'PLAYING':
+                transport_state = 'PLAYING'
+            if result == 'PAUSED':                
+                transport_state = 'PAUSED_PLAYBACK'
 
-        elif t == gst.MESSAGE_EOS:
-            print "reached file end"
-            self.seek('-0')
-            self.update()
+            if self.state != transport_state:
+                self.state = transport_state                             
+                if self.server != None:
+                    self.server.av_transport_server.set_variable(connection_id,
+                                                 'TransportState', transport_state) 
+                                                        
+        dfr = self.player.callRemote("get_readable_state")
+        dfr.addCallback(got_result)
         
+
     def query_position( self):
         #print "query_position"
         try:
@@ -274,62 +253,25 @@ class ElisaPlayer:
                             -nL = relative seek backwards n seconds
         """
         
-        _,state,_ = self.player.get_state()
-        if state != gst.STATE_PAUSED:
-            self.player.set_state(gst.STATE_PAUSED)
-        l = long(location)*1000000000
-        p = self.query_position()
-        
-        #print p['raw']['position'], l
 
-        if location[0] == '+':
-            l =  long(p[u'raw'][u'position']) + (long(location[1:])*1000000000)
-            l = min( l, long(p[u'raw'][u'duration']))
-        if location[0] == '-':
-            if location == '-0':
-                l = 0L
-            else:
-                l = long(p[u'raw'][u'position']) - (long(location[1:])*1000000000)
-                l = max( l, 0L)
-
-
-        print "seeking to %r" % l
-        """
-        self.player.seek( 1.0, gst.FORMAT_TIME,
-            gst.SEEK_FLAG_FLUSH | gst.SEEK_FLAG_ACCURATE,
-            gst.SEEK_TYPE_SET, l,
-            gst.SEEK_TYPE_NONE, 0)
-
-        """
-        event = gst.event_new_seek(1.0, gst.FORMAT_TIME,
-            gst.SEEK_FLAG_FLUSH | gst.SEEK_FLAG_KEY_UNIT,
-            gst.SEEK_TYPE_SET, l,
-            gst.SEEK_TYPE_NONE, 0)
-
-        res = self.player.send_event(event)
-        if res:
-            pass
-            #print "setting new stream time to 0"
-            #self.player.set_new_stream_time(0L)
-        else:
-            print "seek to %r failed" % location
-
-        if location == '-0':
-            self.player.set_state(gst.STATE_READY)
-        else:
-            self.player.set_state(state)
-            if state == gst.STATE_PAUSED:
-                self.update()
 
     def mute(self):
-        self.player.callRemote("mute")
-        rcs_id = self.server.connection_manager_server.lookup_rcs_id(self.current_connection_id)
-        self.server.rendering_control_server.set_variable(rcs_id, 'Mute', 'True')
+        def got_result(result):
+            rcs_id = self.server.connection_manager_server.lookup_rcs_id(self.current_connection_id)
+            #FIXME: use result, not True
+            self.server.rendering_control_server.set_variable(rcs_id, 'Mute', 'True')
+
+        dfr=self.player.callRemote("mute")
+        dfr.addCallback(got_result)
         
     def unmute(self):
-        self.player.callRemote("un_mute")
-        rcs_id = self.server.connection_manager_server.lookup_rcs_id(self.current_connection_id)
-        self.server.rendering_control_server.set_variable(rcs_id, 'Mute', 'False')
+        def got_result(result):
+            rcs_id = self.server.connection_manager_server.lookup_rcs_id(self.current_connection_id)
+            #FIXME: use result, not False
+            self.server.rendering_control_server.set_variable(rcs_id, 'Mute', 'False')
+
+        dfr=self.player.callRemote("un_mute")
+        dfr.addCallback(got_result)
         
     def get_mute(self):
         def got_infos(result):
@@ -357,9 +299,14 @@ class ElisaPlayer:
             volume=0
         if volume > 100:
             volume=100
-        self.player.callRemote("set_volume",volume)
-        rcs_id = self.server.connection_manager_server.lookup_rcs_id(self.current_connection_id)
-        self.server.rendering_control_server.set_variable(rcs_id, 'Volume', volume)
+            
+        def got_result(result):
+            rcs_id = self.server.connection_manager_server.lookup_rcs_id(self.current_connection_id)
+            #FIXME: use result, not volume
+            self.server.rendering_control_server.set_variable(rcs_id, 'Volume', volume)
+                
+        dfr=self.player.callRemote("set_volume",volume)
+        dfr.addCallback(got_result)
         
     def upnp_init(self):
         self.current_connection_id = None
@@ -370,7 +317,8 @@ class ElisaPlayer:
         self.server.av_transport_server.set_variable(0, 'CurrentTransportActions', '', default=True)
         self.server.rendering_control_server.set_variable(0, 'Volume', self.get_volume())
         self.server.rendering_control_server.set_variable(0, 'Mute', self.get_mute())
-
+        self.poll_LC.start( 1.0, True)
+        
     def upnp_Play(self, *args, **kwargs):
         InstanceID = int(kwargs['InstanceID'])
         Speed = int(kwargs['Speed'])
@@ -425,6 +373,7 @@ def main():
     def call_player():
         f.get_volume()
         f.get_mute()
+        f.poll_player()
     
     reactor.callLater(2,call_player)
     
