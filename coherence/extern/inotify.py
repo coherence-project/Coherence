49909ee74e062daa8f3efcf5e99425d043b5a9c2
Frank Scholz <dev@coherence-project.org>
2007-05-10 16:30:21 +0000

    added system call ids for ARM/Maemo platform

--- coherence/extern/inotify.py
+++ coherence/extern/inotify.py
@@ -35,8 +35,8 @@ IN_DELETE =         0x00000200L     # Subfile was delete
 IN_DELETE_SELF =    0x00000400L     # Self was deleted
 IN_MOVE_SELF =      0x00000800L     # Self was moved
 IN_UNMOUNT =        0x00002000L     # Backing fs was unmounted
-IN_Q_OVERFLOW =     0x00004000L     # Event queued overflowed 
-IN_IGNORED =        0x00008000L     # File was ignored 
+IN_Q_OVERFLOW =     0x00004000L     # Event queued overflowed
+IN_IGNORED =        0x00008000L     # File was ignored
 
 IN_ONLYDIR =         0x01000000      # only watch the path if it is a directory
 IN_DONT_FOLLOW =     0x02000000      # don't follow a sym link
@@ -75,13 +75,15 @@ _flag_to_human = {
     IN_MASK_ADD: 'mask_add',
     IN_ISDIR: 'is_dir',
     IN_ONESHOT: 'one_shot'}
-    
+
 _inotify_syscalls = { 'i386': (291,292,293),  # FIXME, there has to be a better way for this
                       'i486': (291,292,293),
                       'i586': (291,292,293),
                       'i686': (291,292,293),
+                      'armv6l':(316,317,318),              # Nokia N800
+                      'armv5tej1':(316,317,318),           # Nokia N770
                       }
-                      
+
 
 class IWatchPoint:
 
@@ -94,10 +96,10 @@ class IWatchPoint:
             if not isinstance(callbacks, list):
                 callbacks = [callbacks]
             self.callbacks = callbacks
-        
+
     def add_callback(self, callback, parameter=None):
         self.callbacks.append((callback,parameter))
-        
+
     def remove_callback(self, callback):
         try:
             del self.callbacks[callback]
@@ -125,7 +127,7 @@ class INotify(FileDescriptor, object):
                 obj.libc = ctypes.CDLL("libc.so.6")
             except:
                 raise SystemError, "libc not found, INotify support disabled"
-                
+
             machine = platform.machine()
             try:
                 obj._init_syscall_id = _inotify_syscalls[machine][0]
@@ -133,7 +135,7 @@ class INotify(FileDescriptor, object):
                 obj._rm_watch_syscall_id = _inotify_syscalls[machine][2]
             except:
                 raise SystemError, "unknown system, INotify support disabled"
-        
+
             FileDescriptor.__init__(obj)
 
             obj._fd = obj.inotify_init()
@@ -141,7 +143,7 @@ class INotify(FileDescriptor, object):
                 raise SystemError, "INotify support not detected on this system."
 
             fdesc.setNonBlocking(obj._fd) # FIXME do we need this?
-            
+
             reactor.addReader(obj)
 
             obj._buffer = ''
@@ -157,13 +159,13 @@ class INotify(FileDescriptor, object):
 
     def inotify_add_watch(self, path, mask):
         return self.libc.syscall(self._add_watch_syscall_id, self._fd, path, mask)
-        
+
     def inotify_rm_watch(self, wd):
         return self.libc.syscall(self._rm_watch_syscall_id, self._fd, wd)
 
     def fileno(self):
         return self._fd
-        
+
     def flag_to_human(self, mask):
         s = []
         for (k, v) in _flag_to_human.iteritems():
@@ -189,7 +191,7 @@ class INotify(FileDescriptor, object):
                 name = None
 
             self._buffer = self._buffer[16+size:]
-            
+
             try:
                 iwp = self._watchpoints[wd]
             except:
@@ -198,9 +200,9 @@ class INotify(FileDescriptor, object):
             path = iwp.path
             if name:
                 path = os.path.join(path, name)
-                
+
             iwp.notify( name, mask)
-            
+
             if( iwp.auto_add and mask & IN_ISDIR and mask & IN_CREATE):
                 self.watch(path, mask = iwp.mask, auto_add = True, callbacks=iwp.callbacks)
 
@@ -216,14 +218,14 @@ class INotify(FileDescriptor, object):
         for wd, iwp in self._watchpoints.items():
             if iwp.path == path:
                 return wd
-                
+
         mask = mask | IN_DELETE_SELF
 
         #print "add watch for", path, ', '.join(self.flag_to_str(mask))
         wd = self.inotify_add_watch(path, mask)
         if wd < 0:
             raise IOError, "Failed to add watch on '%s'" % path
-            
+
         iwp = IWatchPoint(path, mask, auto_add, callbacks)
         self._watchpoints[wd] = iwp
 
