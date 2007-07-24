a78ac6b6b74283061f4cc63af6d33a0334fba397
Frank Scholz <dev@coherence-project.org>
2007-07-24 21:45:16 +0000

    added a namespace identifier to the event notification xml elements, closes #93

--- coherence/upnp/core/service.py
+++ coherence/upnp/core/service.py
@@ -310,11 +310,11 @@ class ServiceServer(log.Loggable):
         if len(notify) <= 0:
             return
 
-        root = ET.Element('propertyset')
-        root.attrib['xmlns']='urn:schemas-upnp-org:event-1-0'
+        root = ET.Element('e:propertyset')
+        root.attrib['xmlns:e']='urn:schemas-upnp-org:event-1-0'
         evented_variables = 0
         for n in notify:
-            e = ET.SubElement( root, 'property')
+            e = ET.SubElement( root, 'e:property')
             if n.name == 'LastChange':
                 text = self.build_last_change_event(n.instance)
                 if text is not None:
@@ -386,9 +386,9 @@ class ServiceServer(log.Loggable):
             return None
 
     def build_single_notification(self, instance, variable_name, value):
-        root = ET.Element('propertyset')
-        root.attrib['xmlns']='urn:schemas-upnp-org:event-1-0'
-        e = ET.SubElement( root, 'property')
+        root = ET.Element('e:propertyset')
+        root.attrib['xmlns:e']='urn:schemas-upnp-org:event-1-0'
+        e = ET.SubElement( root, 'e:property')
         s = ET.SubElement( e, variable_name).text = str(value)
         return ET.tostring( root, encoding='utf-8')
 
@@ -424,15 +424,15 @@ class ServiceServer(log.Loggable):
         if len(notify) <= 0:
             return
 
-        root = ET.Element('propertyset')
-        root.attrib['xmlns']='urn:schemas-upnp-org:event-1-0'
+        root = ET.Element('e:propertyset')
+        root.attrib['xmlns:e']='urn:schemas-upnp-org:event-1-0'
 
         if isinstance( notify, variable.StateVariable):
             notify = [notify,]
 
         evented_variables = 0
         for n in notify:
-            e = ET.SubElement( root, 'property')
+            e = ET.SubElement( root, 'e:property')
             if n.name == 'LastChange':
                 text = self.build_last_change_event(instance=n.instance)
                 if text is not None:
