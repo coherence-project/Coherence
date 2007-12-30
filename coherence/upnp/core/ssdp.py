31f538d01fb5ceac2b8a03cd20c34229599ec4f1
Frank Scholz <dev@coherence-project.org>
2007-12-30 16:54:04 +0000

    move the 'UPnP.SSDP.datagram_received' signal to a place __after__ we had a change to register the device with the SSDP server

--- coherence/upnp/core/ssdp.py
+++ coherence/upnp/core/ssdp.py
@@ -59,9 +59,6 @@ class SSDPServer(DatagramProtocol, log.Loggable):
     def datagramReceived(self, data, (host, port)):
         """Handle a received multicast datagram."""
 
-        #make raw data available
-        louie.send('UPnP.SSDP.datagram_received', None, data, host, port)
-
         try:
             header, payload = data.split('\r\n\r\n')[:2]
         except ValueError, err:
@@ -87,6 +84,10 @@ class SSDPServer(DatagramProtocol, log.Loggable):
             self.notifyReceived(headers, (host, port))
         else:
             self.warning('Unknown SSDP command %s %s' % (cmd[0], cmd[1]))
+
+        # make raw data available
+        # send out the signal after we had a chance to register the device
+        louie.send('UPnP.SSDP.datagram_received', None, data, host, port)
 
     def register(self, manifestation, usn, st, location,
                         server=SERVER_ID,
