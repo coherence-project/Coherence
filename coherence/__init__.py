57e03f639a16c8f5a639dd24bd5919e0aa714f32
Frank Scholz <dev@coherence-project.org>
2008-04-02 10:15:53 +0000

    remove broken 0.5.4 tag

--- coherence/__init__.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-__version_info__ = (0,5,4)
-__version__ = '%d.%d.%d' % (__version_info__[0],__version_info__[1],__version_info__[2],)
-
-SERVER_ID = ','.join([platform.system(),
-                      platform.release(),
-                      'UPnP/1.0,Coherence UPnP framework',
-                      __version__])
