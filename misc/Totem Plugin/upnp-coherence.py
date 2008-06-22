12d36c186ce5c79d197b84172791efe9e1c9c34a
Frank Scholz <dev@coherence-project.org>
2008-06-22 12:23:18 +0000

    change the way a file in Totem is 'loaded' and 'played', closes #144

--- misc/Totem Plugin/upnp-coherence.py
+++ misc/Totem Plugin/upnp-coherence.py
@@ -16,7 +16,12 @@ class UPnPClient(totem.Plugin):
 
     def activate (self, totem_object):
         totem_object.add_sidebar_page ("upnp-coherence", _("Coherence DLNA/UPnP Client"), self.ui.window)
-        self.ui.cb_item_dbl_click =  totem_object.action_set_mrl_and_play
+
+        def load_and_play(url):
+            totem_object.action_remote(totem.REMOTE_COMMAND_REPLACE,url)
+            totem_object.action_remote(totem.REMOTE_COMMAND_PLAY,url)
+            
+        self.ui.cb_item_dbl_click = load_and_play
 
     def deactivate (self, totem_object):
         totem_object.remove_sidebar_page ("upnp-coherence")
