1a5df0231c3f517cd8445df8250a76f63be9745c
Frank Scholz <dev@coherence-project.org>
2008-12-08 17:35:35 +0000

    some cleanups and album-art support

--- coherence/backends/swr3_storage.py
+++ coherence/backends/swr3_storage.py
@@ -5,6 +5,9 @@
 
 # Copyright 2008 Frank Scholz <coherence@beebits.net>
 
+from datetime import datetime
+from email.Utils import parsedate_tz
+
 from coherence.backend import BackendStore
 from coherence.backend import BackendItem
 from coherence.upnp.core import DIDLLite
@@ -16,23 +19,11 @@ from twisted.python.util import OrderedDict
 from coherence.extern.et import parse_xml
 
 ROOT_CONTAINER_ID = 0
-MARIANNE_014_CONTAINER_ID = 100
-PETER_GEDOENS_CONTAINER_ID = 101
-BESCHEIDWISSER_CONTAINER_ID = 102
-TIM_FRAGT_TOM_CONTAINER_ID = 103
-WIE_WAR_DER_TAG_CONTAINER_ID = 104
-BOERSENMAN_CONTAINER_ID = 105
-GAG_DES_TAGES_CONTAINER_ID = 106
-TOPTHEMA_CONTAINER_ID = 107
-EVISHOW_CONTAINER_ID = 108
-REUSCHS_WOCHENRUECKBLICK_CONTAINER_ID = 109
-TAEGLICH_POP_CONTAINER_ID = 110
-
 
 class Item(BackendItem):
 
-    def __init__(self, parent_id, id, title, url):
-        self.parent_id = parent_id
+    def __init__(self, parent, id, title, url):
+        self.parent = parent
         self.id = id
         self.location = url
         self.name = title
@@ -40,13 +31,18 @@ class Item(BackendItem):
         self.size = None
         self.mimetype = 'audio/mpeg'
         self.description = None
+        self.date = None
 
         self.item = None
 
     def get_item(self):
         if self.item == None:
-            self.item = DIDLLite.AudioItem(self.id, self.parent_id, self.name)
+            self.item = DIDLLite.AudioItem(self.id, self.parent.id, self.name)
             self.item.description = self.description
+            self.item.date = self.date
+
+            if hasattr(self.parent, 'cover'):
+                self.item.albumArtURI = self.parent.cover
 
             res = DIDLLite.Resource(self.location, 'http-get:*:%s:*' % self.mimetype)
             res.duration = self.duration
@@ -126,94 +122,19 @@ class SWR3Store(BackendStore):
         self.store[ROOT_CONTAINER_ID] = \
                         Container(ROOT_CONTAINER_ID,self,-1, self.name)
 
-        self.store[MARIANNE_014_CONTAINER_ID] = \
-                        Container(MARIANNE_014_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Mariane 014 - Die Landärztin')
-        self.store[MARIANNE_014_CONTAINER_ID].description = u"""Hier kommt die Rettung aller Landwirte: Comedy-Star Anke Engelke ist „Die Landärztin“ – exklusiv bei SWR3. """ \
-                                                            u"""Als Marianne 014 eilt sie auf geilen Fahrzeugen von einem heißen Notarzt-Einsatz zum nächsten."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[MARIANNE_014_CONTAINER_ID])
-
-        self.store[PETER_GEDOENS_CONTAINER_ID] = \
-                        Container(PETER_GEDOENS_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Peter Gedöns aus Bonn')
-        self.store[PETER_GEDOENS_CONTAINER_ID].description = u"""Sascha Zeus fürchtet seinen Anruf in der SWR3-Morningshow – doch immer wieder meldet er """ \
-                                                             u"""sich per Telefon: Peter Gedöns aus Bonn. Er ist einfach immer schlecht gelaunt und hat """ \
-                                                             u"""immer ein paar wüste Beschimpfungen parat."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[PETER_GEDOENS_CONTAINER_ID])
-
-        self.store[BESCHEIDWISSER_CONTAINER_ID] = \
-                        Container(BESCHEIDWISSER_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Der SWR3-Bescheidwisser')
-        self.store[BESCHEIDWISSER_CONTAINER_ID].description = u"""Die Welt ist voller kleiner Wunder. Können wir Blicke spüren? """ \
-                                                              u"""Was rettet uns, wenn wir uns verschlucken? Warum können wir auf der ganzen Welt Geld abheben? """ \
-                                                              u"""SWR3-Reporter Andreas Hain klärt Phänomene des Alltags auf."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[BESCHEIDWISSER_CONTAINER_ID])
-
-        self.store[TIM_FRAGT_TOM_CONTAINER_ID] = \
-                        Container(TIM_FRAGT_TOM_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Tim fragt Tom')
-        self.store[TIM_FRAGT_TOM_CONTAINER_ID].description = u"""Die Welt ist kompliziert – nicht nur für Kinder. In der SWR3-Serie „Tim fragt Tom“ """ \
-                                                             u"""erklärt der ARD-Tagesthemen-Moderator Tom Buhrow dem 12-jährigen Tim schwierige Begriffe aus den Nachrichten."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[TIM_FRAGT_TOM_CONTAINER_ID])
-
-        self.store[WIE_WAR_DER_TAG_CONTAINER_ID] = \
-                        Container(WIE_WAR_DER_TAG_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Wie war der Tag, Liebling?')
-        self.store[WIE_WAR_DER_TAG_CONTAINER_ID].description = u"""SWR3-Moderator Kristian Thees hat früher mit Anke Engelke zusammen Radio gemacht """ \
-                                                               u"""und ruft sie jetzt regelmäßig aus seiner Sendung an. Worüber die beiden dann plaudern, """ \
-                                                               u"""hört ihr im SWR3-Podcast „Wie war der Tag, Liebling?“"""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[WIE_WAR_DER_TAG_CONTAINER_ID])
-
-        self.store[BOERSENMAN_CONTAINER_ID] = \
-                        Container(BOERSENMAN_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'SWR3-Börsenman')
-        self.store[BOERSENMAN_CONTAINER_ID].description = u"""Ob Aktie des Tages, allgemeine Trends oder Börsengossip: """ \
-                                                          u"""Jeden Tag gibt es eine neue Story vom SWR3 Börsenman. """ \
-                                                          u"""Frisch vom Parkett auf SWR3 und hier zum Nachhören für zuhause und unterwegs."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[BOERSENMAN_CONTAINER_ID])
-
-        self.store[GAG_DES_TAGES_CONTAINER_ID] = \
-                        Container(GAG_DES_TAGES_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'SWR3-Gag des Tages')
-        self.store[GAG_DES_TAGES_CONTAINER_ID].description = u"""Die SWR3-Witzküche arbeitet unentwegt an eurer guten Laune und liefert""" \
-                                                             u"""euch mit dem SWR3-Gag des Tages die besten Comix im Abo: Sachen, die Lachen """ \
-                                                             u"""machen – zum Nachkichern und Mitnehmen."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[GAG_DES_TAGES_CONTAINER_ID])
-
-        self.store[TOPTHEMA_CONTAINER_ID] = \
-                        Container(TOPTHEMA_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'SWR3-Topthema')
-        self.store[TOPTHEMA_CONTAINER_ID].description = u"""Das SWR3 Topthema ist der tägliche Info-Schwerpunkt in der SWR3-Nachmittagsshow """ \
-                                                        u"""– immer gegen 17 Uhr 40 in SWR3 und auf SWR3.de. Damit seid ihr in vier Minuten """ \
-                                                        u"""bestens informiert über die wichtigen Themen."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[TOPTHEMA_CONTAINER_ID])
-
-        self.store[EVISHOW_CONTAINER_ID] = \
-                        Container(EVISHOW_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'SWR3-Evishow – die Interviews')
-        self.store[EVISHOW_CONTAINER_ID].description = u"""SWR3-Moderatorin Evi Seibert angelt sich die angesagtesten Promis der Woche. """ \
-                                                       u"""Sie weiß, wie sie ihre Gäste zum Plaudern bringt. Die Interviews aus ihrer """ \
-                                                       u"""witzig-charmanten Show (Sonntags, 9-12 Uhr) gibt's hier als Podcast."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[EVISHOW_CONTAINER_ID])
-
-        self.store[REUSCHS_WOCHENRUECKBLICK_CONTAINER_ID] = \
-                        Container(REUSCHS_WOCHENRUECKBLICK_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Reuschs rigoroser Wochenrückblick')
-        self.store[REUSCHS_WOCHENRUECKBLICK_CONTAINER_ID].description = u"""Eine ganze Woche im Schnelldurchlauf, und dabei bringt er zusammen, """ \
-                                                                        u"""was zusammen gehört – oder auch nicht: Jeden Freitag läuft in SWR3 der """ \
-                                                                        u"""rigorose Wochenrückblick von Stefan Reusch, und jede Woche gibt's ihn auch als Podcast."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[REUSCHS_WOCHENRUECKBLICK_CONTAINER_ID])
-
-        self.store[TAEGLICH_POP_CONTAINER_ID] = \
-                        Container(TAEGLICH_POP_CONTAINER_ID,self,ROOT_CONTAINER_ID, u'Täglich Pop')
-        self.store[TAEGLICH_POP_CONTAINER_ID].description = u"""SWR3: Täglich Pop - jeden Tag neue Stories rund um eure Stars. Was geschah heute vor zwei Jahren?""" \
-                                                            u"""SWR3-Musikredakteur Matthias Kugler traegt Tag für Tag die interessantesten Facts""" \
-                                                            u"""und die besten Stories für euch zusammen."""
-        self.store[ROOT_CONTAINER_ID].add_child(self.store[TAEGLICH_POP_CONTAINER_ID])
-
         self.init_completed()
 
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/marianne014.xml.php",self.store[MARIANNE_014_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/gedoens.xml.php",self.store[PETER_GEDOENS_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/bescheid.xml.php",self.store[BESCHEIDWISSER_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/timtom.xml.php",self.store[TIM_FRAGT_TOM_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/wwdtl.xml.php",self.store[WIE_WAR_DER_TAG_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/boersenman.xml.php",self.store[BOERSENMAN_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/gag.xml.php",self.store[GAG_DES_TAGES_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/tt.xml.php",self.store[TOPTHEMA_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/evishow.xml.php",self.store[EVISHOW_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/reusch.xml.php",self.store[REUSCHS_WOCHENRUECKBLICK_CONTAINER_ID])
-        self.update_data("http://www.swr3.de/rdf-feed/podcast/taepo.xml.php",self.store[TAEGLICH_POP_CONTAINER_ID])
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/marianne014.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/gedoens.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/bescheid.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/timtom.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/wwdtl.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/boersenman.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/gag.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/tt.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/evishow.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/reusch.xml.php",self.get_next_id())
+        self.update_data("http://www.swr3.de/rdf-feed/podcast/taepo.xml.php",self.get_next_id())
 
     def get_next_id(self):
         self.next_id += 1
@@ -251,10 +172,18 @@ class SWR3Store(BackendStore):
 
     def parse_data(self,xml_data,container):
         root = xml_data.getroot()
+
+        self.store[container] = \
+                        Container(container,self,ROOT_CONTAINER_ID, unicode(root.find("./channel/title").text))
+        self.store[container].description = unicode(root.find("./channel/description").text)
+        self.store[container].cover = root.find("./channel/image/url").text
+        self.store[ROOT_CONTAINER_ID].add_child(self.store[container])
+
         for podcast in root.findall("./channel/item"):
-            item = Item(container.id, self.get_next_id(), unicode(podcast.find("./title").text), podcast.find("./link").text)
-            container.add_child(item)
+            item = Item(self.store[container], self.get_next_id(), unicode(podcast.find("./title").text), podcast.find("./link").text)
+            self.store[container].add_child(item)
             item.description = unicode(podcast.find("./description").text)
+            #item.date = datetime(*parsedate_tz(podcast.find("./pubDate").text)[0:6])
             enclosure = podcast.find("./enclosure")
             item.size = int(enclosure.attrib['length'])
             item.mimetype = enclosure.attrib['type']
