3cff1d9b3c69e2e2c08487d0dbafbcb3f356e55b
Frank Scholz <dev@coherence-project.org>
2009-05-12 20:45:39 +0000

    bumping version to 0.6.5

--- coherence/__init__.py
+++ coherence/__init__.py
@@ -1,6 +1,6 @@
 import platform
 
-__version_info__ = (0,6,4)
+__version_info__ = (0,6,5)
 __version__ = '%d.%d.%d' % (__version_info__[0],__version_info__[1],__version_info__[2],)
 
 SERVER_ID = ','.join([platform.system(),
