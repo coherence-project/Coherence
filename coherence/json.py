c827ab6f499e04880ac0bb22d040bb64f4651253
Frank Scholz <dev@coherence-project.org>
2009-10-06 19:12:18 +0000

    adding first JSON calls:
     * `http://host:port/json/devices` returns a list of device dicts
     * `!http://host:port/json/<uuid>/<service>/<action>?arguments` returns the result of the action call as a dict of the out arguments[[BR]]
       e.g. `!http://host:port/json/<uuid>/<service>/ContentDirectory/Browse?ObjectID=0&BrowseFlag=BrowseDirectChildren&Filter=*&StartingIndex=0&RequestedCount=0&SortCriteria=`

--- coherence/json.py
+++ coherence/json.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# HifiMedia
-# for HIFIakademie.de devices
-
-# Copyright 2009, Frank Scholz <fs@beebits.net>
+# Licensed under the MIT license
+# http://opensource.org/licenses/mit-license.php
 
 import simplejson as json
 from twisted.web import resource,static
+from twisted.internet import defer
 
 from coherence import log
 
 class JsonInterface(resource.Resource,log.Loggable):
     logCategory = 'json'
+    #isLeaf = False
 
     def __init__(self, controlpoint):
         self.controlpoint = controlpoint
@@ -19,11 +19,42 @@ class JsonInterface(resource.Resource,log.Loggable):
                                         self)
         self.children = {}
 
-    def getChildWithDefault(self, path, request):
-        self.warning('getChildWithDefault, %s, %s, %s %s' % (request.method, path, request.uri, request.client))
-        if request.method == 'GET':
-            if path == 'devices':
-                return self.list_devices(request)
+    def render_GET(self,request):
+        d = defer.maybeDeferred(self.do_the_render,request)
+        return d
+
+    def render_POST(self,request):
+        d = defer.maybeDeferred(self.do_the_render,request)
+        return d
+
+    def getChildWithDefault(self,path,request):
+        self.info('getChildWithDefault, %s, %s, %s %s %r' % (request.method, path, request.uri, request.client,request.args))
+        #return self.do_the_render(request)
+        d = defer.maybeDeferred(self.do_the_render,request)
+        return d
+
+    def do_the_render(self,request):
+        self.warning('do_the_render, %s, %s, %s %r %s' % (request.method, request.path,request.uri, request.args, request.client))
+        path = request.path.split('/')
+        path = path[2:]
+        self.warning('path %r' % path)
+        if request.method in ('GET','POST'):
+            request.postpath = None
+            if request.method == 'GET':
+                if path[0] == 'devices':
+                    return self.list_devices(request)
+                else:
+                    device = self.controlpoint.get_device_with_id(path[0])
+                    if device != None:
+                        service = device.get_service_by_type(path[1])
+                        if service != None:
+                            action = service.get_action(path[2])
+                            if action != None:
+                                return self.call_action(action,request)
+
+        request.setResponseCode(404)
+        return static.Data("<html><p>Houston, we've got a problem</p></html>",'text/html')
+
 
 
     def list_devices(self,request):
@@ -31,3 +62,21 @@ class JsonInterface(resource.Resource,log.Loggable):
         for device in self.controlpoint.get_devices():
             devices.append(device.as_dict())
         return static.Data(json.dumps(devices),'application/json')
+
+    def call_action(self,action,request):
+        kwargs = {}
+        for entry,value_list in request.args.items():
+            kwargs[entry] = unicode(value_list[0])
+
+        def to_json(result):
+            self.warning("to_json")
+            return static.Data(json.dumps(result),'application/json')
+
+        def fail(f):
+            request.setResponseCode(404)
+            return static.Data("<html><p>Houston, we've got a problem</p></html>",'text/html')
+
+        d = action.call(**kwargs)
+        d.addCallback(to_json)
+        d.addErrback(fail)
+        return d
