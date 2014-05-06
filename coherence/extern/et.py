1047a981f3ea3834339bdc6a41c369d148307446
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-05-06 20:14:12 +0200

    Cleanup: unused code, minor simplifications, wrap lines and comments.

--- coherence/extern/et.py
+++ coherence/extern/et.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-
+#
 # Licensed under the MIT license
 # http://opensource.org/licenses/mit-license.php
-
-# Copyright 2006,2007 Frank Scholz <coherence@beebits.net>
 #
-# a little helper to get the proper ElementTree package
+# Copyright 2006,2007 Frank Scholz <coherence@beebits.net>
+# Copyright 2014 Hartmut Goebel <h.goebel@crazy-compilers.com>
+#
+"""
+little helper to get the proper ElementTree package
+"""
 
 import re
 import exceptions
@@ -13,53 +16,33 @@ import exceptions
 try:
     import cElementTree as ET
     import elementtree
-    #print "ElementTree: using cElementTree as ET"
 except ImportError:
     try:
         from elementtree import ElementTree as ET
         import elementtree
-        #print "ElementTree: using elementtree.ElementTree as ET"
     except ImportError:
-        """ this seems to be necessary with the python2.5 on the Maemo platform """
+        # this seems to be necessary with the python2.5 on the Maemo platform
         try:
             from xml.etree import cElementTree as ET
             from xml import etree as elementtree
-            #print "ElementTree: using xml.etree.cElementTree as ET"
         except ImportError:
             try:
                 from xml.etree import ElementTree as ET
                 from xml import etree as elementtree
-                #print "ElementTree: using xml.etree.ElementTree as ET"
             except ImportError:
-                #print "ElementTree: no ElementTree module found, critical error"
-                raise ImportError("ElementTree: no ElementTree module found, critical error")
+                raise ImportError("ElementTree: no ElementTree module found, "
+                                  "critical error")
 
 utf8_escape = re.compile(eval(r'u"[&<>\"]+"'))
 escape = re.compile(eval(r'u"[&<>\"\u0080-\uffff]+"'))
 
 
-def encode_entity(text, pattern=escape):
-
-    # map reserved and non-ascii characters to numerical entities
-    def escape_entities(m, map=elementtree.ElementTree._escape_map):
-        out = []
-        append = out.append
-        for char in m.group():
-            t = map.get(char)
-            if t is None:
-                t = "&#%d;" % ord(char)
-            append(t)
-        return ''.join(out)
-    try:
-        return elementtree.ElementTree._encode(pattern.sub(escape_entities, text), 'ascii')
-    except TypeError:
-        elementtree.ElementTree._raise_serialization_error(text)
-
-
 def new_encode_entity(text, pattern=utf8_escape):
-
-    # map reserved and non-ascii characters to numerical entities
+    
     def escape_entities(m, map=elementtree.ElementTree._escape_map):
+        """
+        map reserved and non-ascii characters to numerical entities
+        """
         out = []
         append = out.append
         for char in m.group():
@@ -71,14 +54,17 @@ def new_encode_entity(text, pattern=utf8_escape):
             return ''.join(out)
         else:
             return u''.encode('utf-8').join(out)
+
     try:
         if type(text) == unicode:
-            return elementtree.ElementTree._encode(escape.sub(escape_entities, text), 'ascii')
+            return elementtree.ElementTree._encode(
+                escape.sub(escape_entities, text), 'ascii')
         else:
-            return elementtree.ElementTree._encode(utf8_escape.sub(escape_entities, text.decode('utf-8')), 'utf-8')
+            return elementtree.ElementTree._encode(
+                utf8_escape.sub(escape_entities, text.decode('utf-8')), 'utf-8')
     except TypeError:
-
         elementtree.ElementTree._raise_serialization_error(text)
+
 
 elementtree.ElementTree._encode_entity = new_encode_entity
 
@@ -87,7 +73,6 @@ elementtree.ElementTree._encode_entity = new_encode_entity
 #
 # So we just use the XMLTreeBuilder method for now
 # if XMLParser isn't available.
-
 if not hasattr(ET, 'XMLParser'):
     def XMLParser(encoding='utf-8'):
         return ET.XMLTreeBuilder()
@@ -95,22 +80,16 @@ if not hasattr(ET, 'XMLParser'):
     ET.XMLParser = XMLParser
 
 
-def namespace_map_update(namespaces):
-    #try:
-    #    from xml.etree import ElementTree
-    #except ImportError:
-    #    from elementtree import ElementTree
-
-    elementtree.ElementTree._namespace_map.update(namespaces)
+namespace_map_update = elementtree.ElementTree._namespace_map.update
 
 
-class ElementInterface(elementtree.ElementTree._ElementInterface):
-    """ helper class """
+class ElementInterface(elementtree.ElementTree._ElementInterface): pass
 
 
 def indent(elem, level=0):
-    """ generate pretty looking XML, based upon:
-        http://effbot.org/zone/element-lib.htm#prettyprint
+    """
+    generate pretty looking XML, based upon:
+    http://effbot.org/zone/element-lib.htm#prettyprint
     """
     i = "\n" + level * "  "
     if len(elem):
@@ -136,7 +115,7 @@ def parse_xml(data, encoding="utf-8", dump_invalid_data=False):
     # my version of twisted.web returns page_infos as a dictionary in
     # the second item of the data list
     if isinstance(data, (list, tuple)):
-        data, _ = data
+        data = data[0]
 
     try:
         data = data.encode(encoding)
