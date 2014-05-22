3f2d9ce86cc3ec8cfb78e511ec1eb89ee28b19d2
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-05-22 14:31:26 +0200

    Minor cleanup and wrapping long lines in upnp.core.test.test_uuid.

--- coherence/upnp/services/clients/test/test_switch_power_client.py
+++ coherence/upnp/services/clients/test/test_switch_power_client.py
@@ -17,7 +17,7 @@ from twisted.internet.defer import Deferred
 
 from coherence import __version__
 from coherence.base import Coherence
-from coherence.upnp.core.uuid import UUID
+from coherence.upnp.core import uuid
 from coherence.upnp.devices.control_point import DeviceQuery
 
 import coherence.extern.louie as louie
@@ -27,9 +27,16 @@ class TestSwitchPowerClient(unittest.TestCase):
 
     def setUp(self):
         louie.reset()
-        self.coherence = Coherence({'unittest': 'yes', 'logmode': 'error', 'subsystem_log': {'controlpoint': 'error'}, 'controlpoint': 'yes'})
-        self.uuid = UUID()
-        p = self.coherence.add_plugin('SimpleLight', name='test-light-%d' % os.getpid(), uuid=str(self.uuid))
+        self.coherence = Coherence(
+            {'unittest': 'yes',
+             'logmode': 'error',
+             'subsystem_log': {'controlpoint': 'error'},
+             'controlpoint': 'yes'
+             })
+        self.uuid = str(uuid.UUID())
+        p = self.coherence.add_plugin('SimpleLight',
+                                      name='test-light-%d' % os.getpid(),
+                                      uuid=self.uuid)
 
     def tearDown(self):
 
@@ -50,15 +57,16 @@ class TestSwitchPowerClient(unittest.TestCase):
 
         def the_result(r):
             #print "the_result", r
-            self.assertEqual(str(self.uuid), r.udn)
+            self.assertEqual(self.uuid, r.udn)
 
             call = r.client.switch_power.get_status()
-
             def got_answer(r):
                 self.assertEqual(int(r['ResultStatus']), 0)
                 d.callback(None)
 
             call.addCallback(got_answer)
 
-        self.coherence.ctrl.add_query(DeviceQuery('uuid', str(self.uuid), the_result, timeout=10, oneshot=True))
+        self.coherence.ctrl.add_query(
+            DeviceQuery('uuid', self.uuid, the_result, timeout=10,
+                        oneshot=True))
         return d
