7c28b17cc7839b5366c23d0dcd831b31f8c3d907
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-05-27 17:03:37 +0200

    Clean up test_dbus so semantics becomes clear.
    The prior code's semantics was hard to understand due to the deep
    code- nesting. Now the code uses a decorator to wrap the callbacks by
    try-except and ensure errback() is called on failure. Also removed
    useless error-handlers (directly use errback instead).
    
    There still needs to be some clean-up code added, since I get an
    DirtyReactorAggregateError if one of the tests fails.

--- coherence/test/test_dbus.py
+++ coherence/test/test_dbus.py
@@ -17,9 +17,10 @@ from twisted.internet.defer import Deferred
 
 from coherence import __version__
 from coherence.base import Coherence
-from coherence.upnp.core.uuid import UUID
-
+from coherence.upnp.core import uuid
 import coherence.extern.louie as louie
+
+from coherence.test import wrapped
 
 BUS_NAME = 'org.Coherence'
 OBJECT_PATH = '/org/Coherence'
@@ -32,7 +33,7 @@ class TestDBUS(unittest.TestCase):
         self.coherence = Coherence({'unittest': 'yes', 'logmode': 'error', 'use_dbus': 'yes', 'controlpoint': 'yes'})
         self.bus = dbus.SessionBus()
         self.coherence_service = self.bus.get_object(BUS_NAME, OBJECT_PATH)
-        self.uuid = UUID()
+        self.uuid = str(uuid.UUID())
 
     def tearDown(self):
 
@@ -47,49 +48,54 @@ class TestDBUS(unittest.TestCase):
     def test_dbus_version(self):
         """ tests the version number request via dbus
         """
+
         d = Deferred()
 
+        @wrapped(d)
         def handle_version_reply(version):
             self.assertEqual(version, __version__)
             d.callback(version)
 
-        def handle_error(err):
-            d.errback(err)
-
         self.coherence_service.version(dbus_interface=BUS_NAME,
                                        reply_handler=handle_version_reply,
-                                       error_handler=handle_error)
+                                       error_handler=d.errback)
         return d
 
     def test_dbus_plugin_add_and_remove(self):
         """ tests creation and removal of a backend via dbus
         """
+
         d = Deferred()
 
-        def handle_error(err):
-            d.errback(err)
+        @wrapped(d)
+        def add_it(uuid):
+            self.coherence_service.add_plugin(
+                'SimpleLight', {'name': 'dbus-test-light-%d' % os.getpid(),
+                                'uuid': uuid,
+                                },
+                dbus_interface=BUS_NAME,
+                reply_handler=handle_add_plugin_reply,
+                error_handler=d.errback)
 
+        @wrapped(d)
         def handle_add_plugin_reply(uuid):
-            uuid = str(uuid)
-            self.assertEqual(str(self.uuid), uuid)
-
-            def remove_it(uuid):
-
-                def handle_remove_plugin_reply(uuid):
-                    self.assertEqual(str(self.uuid), uuid)
-                    d.callback(uuid)
-
-                self.coherence_service.remove_plugin(uuid,
-                                            dbus_interface=BUS_NAME,
-                                            reply_handler=handle_remove_plugin_reply,
-                                            error_handler=handle_error)
-
+            self.assertEqual(self.uuid, uuid)
             reactor.callLater(2, remove_it, uuid)
 
-        self.coherence_service.add_plugin('SimpleLight', {'name': 'dbus-test-light-%d' % os.getpid(), 'uuid': str(self.uuid)},
-                                          dbus_interface=BUS_NAME,
-                                          reply_handler=handle_add_plugin_reply,
-                                          error_handler=handle_error)
+        @wrapped(d)
+        def remove_it(uuid):
+            self.coherence_service.remove_plugin(
+                uuid,
+                dbus_interface=BUS_NAME,
+                reply_handler=handle_remove_plugin_reply,
+                error_handler=d.errback)
+
+        @wrapped(d)
+        def handle_remove_plugin_reply(uuid):
+            self.assertEqual(self.uuid, uuid)
+            d.callback(uuid)
+
+        add_it(self.uuid)
         return d
 
 if reactor.__class__.__name__ != 'Glib2Reactor':
