5e1d1fc2f32a3971ee3d4cef873e80188c8ca6a2
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-05-27 17:25:41 +0200

    Clean up test_switch_power_client.
    Now the code uses a decorator to wrap the callbacks by try-except
    and ensure errback() is called on failure.

--- coherence/upnp/services/clients/test/test_switch_power_client.py
+++ coherence/upnp/services/clients/test/test_switch_power_client.py
@@ -4,6 +4,7 @@
 # http://opensource.org/licenses/mit-license.php
 
 # Copyright 2008, Frank Scholz <coherence@beebits.net>
+# Copyright 2014 Hartmut Goebel <h.goebel@crazy-compilers.com>
 
 """
 Test cases for L{upnp.services.clients.switch_power_client}
@@ -17,19 +18,26 @@ from twisted.internet.defer import Deferred
 
 from coherence import __version__
 from coherence.base import Coherence
-from coherence.upnp.core.uuid import UUID
+from coherence.upnp.core import uuid
 from coherence.upnp.devices.control_point import DeviceQuery
-
 import coherence.extern.louie as louie
+
+from coherence.test import wrapped
 
 
 class TestSwitchPowerClient(unittest.TestCase):
 
     def setUp(self):
         louie.reset()
-        self.coherence = Coherence({'unittest': 'yes', 'logmode': 'error', 'subsystem_log': {'controlpoint': 'error'}, 'controlpoint': 'yes'})
-        self.uuid = UUID()
-        p = self.coherence.add_plugin('SimpleLight', name='test-light-%d' % os.getpid(), uuid=str(self.uuid))
+        self.coherence = Coherence(
+            {'unittest': 'yes',
+             'logmode': 'error',
+             'subsystem_log': {'controlpoint': 'error'},
+             'controlpoint': 'yes'})
+        self.uuid = str(uuid.UUID())
+        p = self.coherence.add_plugin('SimpleLight',
+                                      name='test-light-%d' % os.getpid(),
+                                      uuid=self.uuid)
 
     def tearDown(self):
 
@@ -48,17 +56,18 @@ class TestSwitchPowerClient(unittest.TestCase):
         """
         d = Deferred()
 
+        @wrapped(d)
         def the_result(r):
-            #print "the_result", r
-            self.assertEqual(str(self.uuid), r.udn)
-
+            self.assertEqual(self.uuid, r.udn)
             call = r.client.switch_power.get_status()
-
-            def got_answer(r):
-                self.assertEqual(int(r['ResultStatus']), 0)
-                d.callback(None)
-
             call.addCallback(got_answer)
 
-        self.coherence.ctrl.add_query(DeviceQuery('uuid', str(self.uuid), the_result, timeout=10, oneshot=True))
+        @wrapped(d)
+        def got_answer(r):
+            self.assertEqual(int(r['ResultStatus']), 0)
+            d.callback(None)
+
+        self.coherence.ctrl.add_query(
+            DeviceQuery('uuid', self.uuid, the_result,
+                        timeout=10, oneshot=True))
         return d
