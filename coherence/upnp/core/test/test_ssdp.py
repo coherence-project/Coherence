41fc2b39e62560a30b7c087a2ae3759d2ed256f8
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-06-03 12:38:51 +0200

    Rework code for test-cases for upnp-core-ssdp a bit.

--- coherence/upnp/core/test/test_ssdp.py
+++ coherence/upnp/core/test/test_ssdp.py
@@ -18,15 +18,17 @@ from coherence.upnp.core import ssdp
 SSDP_PORT = 1900
 SSDP_ADDR = '239.255.255.250'
 
-ssdp_notify_1 = """NOTIFY * HTTP/1.1
-Host:239.255.255.250:1900
-NT:upnp:rootdevice
-NTS:ssdp:alive
-Location:http://10.10.222.94:2869/upnp?content=uuid:e711a4bf
-USN:uuid:e711a4bf::upnp:rootdevice
-Cache-Control:max-age=1800
-Server:Microsoft-Windows-NT/5.1 UPnP/1.0 UPnP-Device-Host/1.0
-""".splitlines()
+USN_1 = 'uuid:e711a4bf::upnp:rootdevice'
+SSDP_NOTIFY_1 = (
+    'NOTIFY * HTTP/1.1',
+    'Host:239.255.255.250:1900',
+    'NT:upnp:rootdevice',
+    'NTS:ssdp:alive',
+    'Location:http://10.10.222.94:2869/upnp?content=uuid:e711a4bf',
+    'USN: ' + USN_1,
+    'Cache-Control: max-age=1842',
+    'Server:Microsoft-Windows-NT/5.1 UPnP/1.0 UPnP-Device-Host/1.0',
+    )
 
 
 class TestSSDP(unittest.TestCase):
@@ -38,19 +40,18 @@ class TestSSDP(unittest.TestCase):
 
     def test_ssdp_notify(self):
         self.assertEqual(self.proto.known, {})
-        data = '\r\n'.join(ssdp_notify_1) + '\r\n\r\n'
+        data = '\r\n'.join(SSDP_NOTIFY_1) + '\r\n\r\n'
         self.proto.datagramReceived(data, ('10.20.30.40', 1234))
-        usn = 'uuid:e711a4bf::upnp:rootdevice'
-        self.assertTrue(self.proto.isKnown(usn))
-        self.assertFalse(self.proto.isKnown(usn*2))
-        service = self.proto.known[usn]
+        self.assertTrue(self.proto.isKnown(USN_1))
+        self.assertFalse(self.proto.isKnown(USN_1*2))
+        service = self.proto.known[USN_1]
         del service['last-seen']
         self.assertEqual(service, {
             'HOST': '10.20.30.40',
             'ST': 'upnp:rootdevice',
             'LOCATION': 'http://10.10.222.94:2869/upnp?content=uuid:e711a4bf',
             'USN': 'uuid:e711a4bf::upnp:rootdevice',
-            'CACHE-CONTROL': 'max-age=1800',
+            'CACHE-CONTROL': 'max-age=1842',
             'SERVER': 'Microsoft-Windows-NT/5.1 UPnP/1.0 UPnP-Device-Host/1.0',
             'MANIFESTATION': 'remote',
             'SILENT': False,
@@ -58,12 +59,12 @@ class TestSSDP(unittest.TestCase):
             })
 
     def test_ssdp_notify_does_not_send_reply(self):
-        data = '\r\n'.join(ssdp_notify_1) + '\r\n\r\n'
+        data = '\r\n'.join(SSDP_NOTIFY_1) + '\r\n\r\n'
         self.proto.datagramReceived(data, ('127.0.0.1', 1234))
         self.assertEqual(self.tr.written, [])
 
     def test_doNotify(self):
-        data = '\r\n'.join(ssdp_notify_1) + '\r\n\r\n'
+        data = '\r\n'.join(SSDP_NOTIFY_1) + '\r\n\r\n'
         self.proto.datagramReceived(data, ('10.20.30.40', 1234))
         self.assertEqual(self.tr.written, [])
         self.proto.doNotify('uuid:e711a4bf::upnp:rootdevice')
@@ -74,8 +75,8 @@ class TestSSDP(unittest.TestCase):
             'NTS: ssdp:alive',
             'EXT: ',
             'LOCATION: http://10.10.222.94:2869/upnp?content=uuid:e711a4bf',
-            'USN: uuid:e711a4bf::upnp:rootdevice',
-            'CACHE-CONTROL: max-age=1800',
+            'USN: ' + USN_1,
+            'CACHE-CONTROL: max-age=1842',
             'SERVER: Microsoft-Windows-NT/5.1 UPnP/1.0 UPnP-Device-Host/1.0',
             ''
             ]]
@@ -88,7 +89,7 @@ class TestSSDP(unittest.TestCase):
         self.assertEqual(sorted(recieved), sorted(expected))
 
     def test_doByebye(self):
-        data = '\r\n'.join(ssdp_notify_1) + '\r\n\r\n'
+        data = '\r\n'.join(SSDP_NOTIFY_1) + '\r\n\r\n'
         self.proto.datagramReceived(data, ('10.20.30.40', 1234))
         self.assertEqual(self.tr.written, [])
         self.proto.doByebye('uuid:e711a4bf::upnp:rootdevice')
@@ -99,8 +100,8 @@ class TestSSDP(unittest.TestCase):
             'NTS: ssdp:byebye',
             'EXT: ',
             'LOCATION: http://10.10.222.94:2869/upnp?content=uuid:e711a4bf',
-            'USN: uuid:e711a4bf::upnp:rootdevice',
-            'CACHE-CONTROL: max-age=1800',
+            'USN: ' + USN_1,
+            'CACHE-CONTROL: max-age=1842',
             'SERVER: Microsoft-Windows-NT/5.1 UPnP/1.0 UPnP-Device-Host/1.0',
             ''
             ]]
