cf5873cc8a263c5dbc369af93eeff34c12ad9e33
Hartmut Goebel <h.goebel@crazy-compilers.com>
2014-06-05 20:59:51 +0200

    Add some first test-cases for upnp.core.action.Action.
    Many more are needed.

--- coherence/upnp/core/test/test_action.py
+++ coherence/upnp/core/test/test_action.py
@@ -46,3 +46,35 @@ class TestArguments(unittest.TestCase):
                           ('Direction', 'in-and-out'),
                           ('Related State Variable', 'Brightness'),
                           ])
+
+
+class TestAction(unittest.TestCase):
+
+    def __build_action_arguments(self):
+        args = [
+            action.Argument('InstanceID', 'in', 'A_ARG_TYPE_InstanceID'),
+            action.Argument('CurrentBrightness', 'out', 'Brightness')
+            ]
+        return args
+
+    def test_action(self):
+        svc = DummyService()
+        arguments = self.__build_action_arguments()
+        act = action.Action(svc, 'SomeAction', NoImplementation,
+                            arguments)
+        self.assertEqual(act.get_name(), 'SomeAction')
+        self.assertIs(act.get_service(), svc)
+        self.assertIs(act.get_implementation(), NoImplementation)
+        self.assertEqual(act.get_arguments_list(), arguments)
+        self.assertEqual(act.get_in_arguments(), [arguments[0]])
+        self.assertEqual(act.get_out_arguments(), [arguments[1]])
+        self.assertIs(act.get_in_arguments()[0], arguments[0])
+        self.assertIs(act.get_out_arguments()[0], arguments[1])
+        self.assertIs(act.get_callback(), None)
+
+    def test_action_set_callback(self):
+        def _this_callback(): pass
+        act = action.Action(None, 'SomeAction', NoImplementation, [])
+        self.assertIs(act.get_callback(), None)
+        act.set_callback(_this_callback)
+        self.assertIs(act.get_callback(), _this_callback)
